41265e7b3a03b49e1922441755aa42b350aef69a
GEORGY <Gura_Programm_Puthon>
2021-02-04 15:17:06 +0300

    1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.persistence.Entity;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        for (String s: args){
+            System.out.println(s);
+        }
+    }
+}
